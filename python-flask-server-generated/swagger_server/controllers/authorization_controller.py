0e8488b0cc2d0e554a535b22fad8eaacecba2d9d
cybigot <cyril.bigot.pro@gmail.com>
2020-04-22 17:26:42 +0200

    Dummy server

--- python-flask-server-generated/swagger_server/controllers/authorization_controller.py
+++ python-flask-server-generated/swagger_server/controllers/authorization_controller.py
@@ -3,8 +3,11 @@ from typing import List
 controller generated to handled auth operation described at:
 https://connexion.readthedocs.io/en/latest/security.html
 """
+
+
 def check_api_key(api_key, required_scopes):
     return {'test_key': 'test_value'}
+
 
 def check_basic_auth(username, password, required_scopes):
     return {'test_key': 'test_value'}
