0f4c4d8be2eef76f5a379626d8342e9b3b4f9470
cybigot <cyril.bigot.pro@gmail.com>
2020-06-15 12:37:21 +0200

    Omim controller with new query New API contract ver 15/06/20 with new OMIM query PARTIALLY adapted working swagger file

--- RDcode_API_server/swagger_server/controllers/authorization_controller.py
+++ RDcode_API_server/swagger_server/controllers/authorization_controller.py
@@ -5,11 +5,8 @@ https://connexion.readthedocs.io/en/latest/security.html
 """
 
 
-def check_apiKey(apiKey, required_scopes):
+def check_api_key(apiKey, required_scopes):
     return {'test_key': 'test_value'}
 
-
-def check_basic_auth(username, password, required_scopes):
-    return {'test_key': 'test_value'}
 
 
--- RDcode_API_server/swagger_server/controllers/omim_controller.py
+++ RDcode_API_server/swagger_server/controllers/omim_controller.py
@@ -1,3 +1,5 @@
+import operator
+
 import connexion
 
 from swagger_server.models.error_model import ErrorModel  # noqa: E501
@@ -6,6 +8,34 @@ from swagger_server import util
 
 import config
 from controllers.query_controller import *
+
+
+def list_omim(lang, orphacode):  # noqa: E501
+    """Search for OMIM code(s) of the clinical entity by its ORPHAcode.
+
+    :param lang: Desired language
+    :type lang: str
+    :param orphacode: A unique and time-stable numerical identifier attributed randomly by the database upon creation of the entity.
+    :type orphacode: int
+
+    :rtype: Icd10
+    """
+    es = config.elastic_server
+
+    index = "rdcode_orpha_omim_mapping"
+    index = "{}_{}".format(index, lang.lower())
+
+    query = "{\"query\": {\"match\": {\"ORPHAcode\": " + str(orphacode) + "}}," \
+            "\"_source\":[\"Date\", \"ORPHAcode\",\"Preferred term\", \"Code OMIM\"]}"
+
+    response = single_res(es, index, query)
+    # Test to return error
+    if isinstance(response, str) or isinstance(response, tuple):
+        return response
+    else:
+        references = response.pop("Code OMIM")
+        response["References"] = references
+    return response
 
 
 def list_orpha_by_omim(lang, omimcode):  # noqa: E501
@@ -20,4 +50,41 @@ def list_orpha_by_omim(lang, omimcode):  # noqa: E501
 
     :rtype: FindbyOMIM
     """
-    return 'Not implemented'
+    es = config.elastic_server
+
+    index = "rdcode_orpha_omim_mapping"
+    index = "{}_{}".format(index, lang.lower())
+
+    # Find every occurrences of the queried ICD code and return the associated Date, ORPHAcode, Preferred term, Refs ICD
+    query = "{\"query\": {\"match\": {\"Code OMIM.Code OMIM\": \"" + str(omimcode) + "\"}}," \
+                         "\"_source\":[\"Date\", \"ORPHAcode\", \"Preferred term\", \"Code OMIM\"]}"
+
+    response_omim_to_orpha = multiple_res(es, index, query, 1000)
+
+    # Test to return error
+    if isinstance(response_omim_to_orpha, str) or isinstance(response_omim_to_orpha, tuple):
+        return response_omim_to_orpha
+    else:
+        response = {}
+        references = []
+        # Source data are organized from the perspective of ORPHA concept
+        # 1 ORPHAcode => X OMIM
+        # response_icd_to_orpha is a list of object containing "Code OMIM"
+        # "Code OMIM" is also a list of object that need to be filtrated by OMIM
+        for ref in response_omim_to_orpha:
+            reference = {"ORPHAcode": int(ref["ORPHAcode"]),
+                         "Preferred term": ref["Preferred term"],
+                         "DisorderMappingRelation": "",
+                         "DisorderMappingValidationStatus": ""}
+            for CodeOMIM in ref["Code OMIM"]:
+                if CodeOMIM["Code OMIM"] == omimcode:
+                    reference["DisorderMappingRelation"] = CodeOMIM["DisorderMappingRelation"]
+                    reference["DisorderMappingValidationStatus"] = CodeOMIM["DisorderMappingValidationStatus"]
+            references.append(reference)
+        # Sort references by Orphacode
+        references.sort(key=operator.itemgetter("ORPHAcode"))
+        # Compose the final response
+        response["Date"] = response_omim_to_orpha[0]["Date"]
+        response["Code OMIM"] = omimcode
+        response["References"] = references
+    return response
