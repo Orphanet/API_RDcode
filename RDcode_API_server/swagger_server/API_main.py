60871aae9cc21d2e5e62b09b549c71747aa90283
cybigot <cyril.bigot.pro@gmail.com>
2020-06-26 18:23:58 +0200

    Enable yaml response

--- RDcode_API_server/swagger_server/API_main.py
+++ RDcode_API_server/swagger_server/API_main.py
@@ -3,11 +3,15 @@ import os
 
 import connexion
 from flask import send_from_directory
+from flask import render_template
+import swagger_ui_bundle
 
 from swagger_server import encoder
 
 
 def main():
+    # swagger_url = path to ui
+    # "swagger_ui_config": {"defaultModelsExpandDepth": -1} = do not display data models
     options = {'swagger_url': '/', "swagger_ui_config": {"defaultModelsExpandDepth": -1}}
     app = connexion.App(__name__, specification_dir='swagger/', options=options)
     app.app.json_encoder = encoder.JSONEncoder
@@ -18,8 +22,15 @@ def main():
         return send_from_directory(os.path.join(app.root_path),
                                    'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+    # @app.route('/')
+    # def ui():
+    #     response = render_template("/new_ui.j2")
+    #     return response
+
     # Force the direct encoding of accents in json
     # app.app.config['JSON_AS_ASCII'] = False
+
+    # Remove A-z sorting in json
     # app.app.config['JSON_SORT_KEYS'] = False
     return app
 
