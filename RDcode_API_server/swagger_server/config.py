d98ad47fdbbe88bae050cd07c3d44092af81c5ac
cybigot <cyril.bigot.pro@gmail.com>
2020-07-02 15:52:22 +0200

    Working version 1.1 data rdcode 2020

--- RDcode_API_server/swagger_server/config.py
+++ RDcode_API_server/swagger_server/config.py
@@ -2,7 +2,7 @@ from elasticsearch import Elasticsearch
 
 # ELASTIC SEARCH
 # Local test
-elastic_server = Elasticsearch(hosts=["localhost"])
+#elastic_server = Elasticsearch(hosts=["localhost"])
 
 # Local test from docker
 # elastic_server = Elasticsearch(hosts=["host.docker.internal"])
@@ -10,12 +10,12 @@ elastic_server = Elasticsearch(hosts=["localhost"])
 # Online
 # Check redmine ticket http://redminor.orpha.net/issues/15752
 # ES endpoint
-# es_url = "https://9d2d8c7975624d95aa964a1d22a96daf.eu-west-1.aws.found.io:9243"
-# es_api_key = {"id": "8Y5gTXIB0AJkm0jhUTep",
-#               "name": "rdcodeapi",
-#               "api_key": "bgP9GTdNQ3C3AkrDUpsNeQ"
-#               }
-# elastic_server = Elasticsearch(hosts=[es_url], api_key=(es_api_key["id"], es_api_key["api_key"]))
+es_url = "https://9d2d8c7975624d95aa964a1d22a96daf.eu-west-1.aws.found.io:9243"
+es_api_key = {"id": "8Y5gTXIB0AJkm0jhUTep",
+              "name": "rdcodeapi",
+              "api_key": "bgP9GTdNQ3C3AkrDUpsNeQ"
+              }
+elastic_server = Elasticsearch(hosts=[es_url], api_key=(es_api_key["id"], es_api_key["api_key"]))
 
 
 scroll_timeout = "2m"
