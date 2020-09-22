0e0cc4af97d28110862bacb14cd6c1303df16ba8
cybigot <cyril.bigot.pro@gmail.com>
2020-09-22 17:57:47 +0200

    Approximate search on preferred term

--- swagger_server/controllers/preferred_term_controller.py
+++ swagger_server/controllers/preferred_term_controller.py
@@ -22,7 +22,37 @@ def list_by_approx_name(lang, label):  # noqa: E501
 
     :rtype: ApproxFindbyName
     """
-    return 'do some magic!'
+    es = config.elastic_server
+
+    index = "rdcode_orphanomenclature"
+    index = "{}_{}".format(index, lang.lower())
+
+    # print(label)
+
+    # Special FUZZY MATCH query
+    if " " in label.strip():
+        query_term_list = []
+        for term in label.split(" "):
+            query_term = "{{\"fuzzy\": {{\"Preferred term\": {{\"value\" :\"{}\", \"fuzziness\": {}}}}}}}".format(term, len(term))
+            # print(query_term)
+            query_term_list.append(query_term)
+        query_term_list = "[" + ",".join(query_term_list) + "]"
+        # print(query_term_list)
+
+        query = "{\"query\": {\"bool\": {\"should\": " + query_term_list + \
+                "}},\"_source\":[\"Date\", \"ORPHAcode\", \"Preferred term\"]}"
+    else:
+        query = "{\"query\": {\"fuzzy\": {\"Preferred term\": {\"value\" :" + "\"{}\"".format(label) + \
+                ", \"fuzziness\": " + "{}".format(len(label)) + "}}}," + \
+                "\"_source\":[\"Date\", \"ORPHAcode\", \"Preferred term\"]}"
+
+    # print(query)
+    response = multiple_res(es, index, query, 10000)
+    # print(response)
+
+    # return yaml if needed
+    response = if_yaml(connexion.request.accept_mimetypes.best, response)
+    return response
 
 
 def list_by_name(lang, label):  # noqa: E501
