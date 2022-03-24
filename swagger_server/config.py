927edfd8f7501c1b3d903efcb65a8bafbf340cc1
nchenche <nicolas.chevrollier@laposte.net>
2022-03-24 12:15:19 +0100

    Update elastic server access

--- swagger_server/config.py
+++ swagger_server/config.py
@@ -17,22 +17,22 @@ es_api_key = {"id": "8Y5gTXIB0AJkm0jhUTep",
               }
 # elastic_server = Elasticsearch(hosts=[es_url], api_key=(es_api_key["id"], es_api_key["api_key"]))
 
-# ELASTIC_URL="https://9d2d8c7975624d95aa964a1d22a96daf.eu-west-1.aws.found.io"
-# ELASTIC_USER="elastic"
-# ELASTIC_PASS="fSowAPgpKjaA3hD6T7NxctEf"
-
-# elastic_server = Elasticsearch(
-#     hosts=ELASTIC_URL,
-#     port=9243,
-#     http_auth=(ELASTIC_USER, ELASTIC_PASS),
-#     timeout=20
-# )
+ELASTIC_URL="https://9d2d8c7975624d95aa964a1d22a96daf.eu-west-1.aws.found.io"
+ELASTIC_USER="elastic"
+ELASTIC_PASS="fSowAPgpKjaA3hD6T7NxctEf"
 
 elastic_server = Elasticsearch(
-    hosts=["localhost"],
-    port=9200,
+    hosts=ELASTIC_URL,
+    port=9243,
+    http_auth=(ELASTIC_USER, ELASTIC_PASS),
     timeout=20
 )
+
+# elastic_server = Elasticsearch(
+#     hosts=["localhost"],
+#     port=9200,
+#     timeout=20
+# )
 
 
 scroll_timeout = "2m"
