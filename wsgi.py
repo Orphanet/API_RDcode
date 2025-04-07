e5ea6e22393625251836ba633054e1ba50feebae
Stéphane Mabille <stephane.mbll@gmail.com>
2025-04-07 17:16:25 +0200

    CORS + title + RD Code

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,20 @@
 from swagger_server.API_main import main
 from flask import Flask, send_from_directory, send_file
+from flask_cors import CORS, cross_origin
 import os
 import connexion
+from swagger_server import encoder
 
 application = connexion.App(__name__)
 application.add_api('./swagger_server/swagger/swagger.yaml', arguments={'title': 'API RDcode'}, pythonic_params=True)
 
-@application.route("/")
+application.json_encoder = encoder.JSONEncoder
+
+# Authorize cors from all sites
+CORS(application.app, methods=["GET"])
+
+@application.route("/", methods=["GET"])
+@cross_origin(methods=["GET"])
 def API():
     return send_from_directory(os.path.join(application.root_path), "static/index.html")
 
