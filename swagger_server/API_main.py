ab96f0f6293fc6bf6771ecbeeaabc8978cd9a00c
Marc Hanauer <marc@broussais.inserm.fr>
2025-07-11 13:17:48 +0200

    modif since full dataset in UTF8

--- swagger_server/API_main.py
+++ swagger_server/API_main.py
@@ -27,7 +27,7 @@ def main():
         return send_from_directory("../media/", image)
 
     # Force the direct encoding of accents in json
-    # app.app.config['JSON_AS_ASCII'] = False
+     app.app.config['JSON_AS_ASCII'] = False
 
     # Remove A-z sorting in json
     app.app.config['JSON_SORT_KEYS'] = False
